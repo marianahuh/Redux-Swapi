dbfdb11c89e882fe61b5773f63b98dc914fd5065
Mariana Huh <mhuh0176@gmail.com>
2019-06-25 20:15:12 -0700

    characterList working and added loader

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,8 +1,10 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { CharacterList } from "../components";
+import { CharacterList } from '../components';
 // import actions
+import { getCharacters } from '../actions/index';
+import Loader from 'react-loader-spinner';
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -11,14 +13,23 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
+    this.props.getCharacters();
   }
 
   render() {
-    if (this.props.fetching) {
+    if (this.props.isFetching) {
       // return something here to indicate that you are fetching data
+      return (
+        <div>
+          <h3>Targeting Star Wars Characters...hold...hold...</h3>
+          {this.props.isFetching && (
+            <Loader type='Bars' color='#somecolor' height={80} width={80} />
+          )}
+        </div>
+      );
     }
     return (
-      <div className="CharactersList_wrapper">
+      <div className='CharactersList_wrapper'>
         <CharacterList characters={this.props.characters} />
       </div>
     );
@@ -27,9 +38,14 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+const mapStateToProps = state => {
+  return {
+    characters: state.charsReducer.characters,
+    error: state.charsReducer.error,
+    isFetching: state.charsReducer.isFetching
+  };
+};
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps /* mapStateToProps replaces null here */,
+  { getCharacters } /* action creators go here */
 )(CharacterListView);
